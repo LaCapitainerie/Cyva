04ccdd998e5fdc7bbe8ee18e76659815f7f347fd
LaCapitainerie <hugo.antreassian@gmail.com>
2024-10-06 14:04:05 +0200

    Finished Initialization

--- Main/hooks copy/use-lang.ts
+++
@@ -1,6 +0,0 @@
-import { lang } from "@/lib/utils";
-import { useLocalStorage } from "./use-local-storage";
-
-export function useLang() {
-    return useLocalStorage<lang>("lang", "fr-Fr");
-}
--- Main/hooks copy/use-user.ts
+++
@@ -1,6 +0,0 @@
-import { Adherent } from "@/type/Adherent";
-import { useLocalStorage } from "./use-local-storage";
-
-export function useUser() {
-    return useLocalStorage<Adherent>("NOMOREWASTEUSER", {} as Adherent);
-}
